e971801da84b6111ccaa610859b07c5c3c1214ab
Raymondriter <rpr2998@gmail.com>
2020-04-29 17:19:36 -0400

    added registration page

--- js/userAuth.js
+++ js/userAuth.js
@@ -3,21 +3,39 @@ const userRegForm = document.querySelector('#userRegForm');
 const signInForm = document.querySelector('#idSignIn');
 
 
-auth.onAuthStateChanged(function(user) {
+auth.onAuthStateChanged(function (user) {
     // user is undefined if no user signed in
-    if(user){
+    if (user) {
         console.log("you are signed in");
-        document.getElementById("idSign").style.display="none";
-        document.getElementById("idReg").style.display="none";
-        document.getElementById("loginBtn").style.display="none";
-        document.getElementById("logoutBtn").style.display="block";
+        document.getElementById("idSign").style.display = "none";
+        document.getElementById("idReg").style.display = "none";
+        document.getElementById("loginBtn").style.display = "none";
+        document.getElementById("logoutBtn").style.display = "block";
 
-    }else {
+
+    } else {
         console.log("you are not signed in");
-        document.getElementById("loginBtn").style.display="block";
-        document.getElementById("logoutBtn").style.display="none";
+        document.getElementById("loginBtn").style.display = "block";
+        document.getElementById("logoutBtn").style.display = "none";
+
+
     }
 });
+
+const userOnline = auth.currentUser;
+
+const miningBtn = document.getElementById('miningBtn');
+if (userOnline) {
+    // User is signed in.
+    miningBtn.onclick = function () {
+
+    }
+
+} else {
+    // No user is signed in.
+
+
+}
 
 userRegForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -26,9 +44,10 @@ userRegForm.addEventListener('submit', (e) => {
     const psw = userRegForm['pswIn'].value;
     const pswRe = userRegForm['pswInRe'].value;
 
-    if(psw !== pswRe){
+
+    if (psw !== pswRe) {
         alert("Passwords do not match");
-    }else {
+    } else {
         auth.createUserWithEmailAndPassword(email, psw).then(cred => {
             console.log(cred);
         }).catch(function (error) {
@@ -37,7 +56,8 @@ userRegForm.addEventListener('submit', (e) => {
             alert(errorMessage);
             console.log(errorCode);
         })
-    }});
+    }
+});
 
 signInForm.addEventListener('submit', (e) => {
     e.preventDefault();
